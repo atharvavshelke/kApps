e51329a25eab69d6290897c7363e0c1802592615
Atharva V. Shelke <avskdkpro+github@gmail.com>
2026-03-04 08:19:52 +0530

    Fix build issues: removed unused puppeteer and sanitized script filenames

--- browser/public/uv.config.js
+++ browser/public/uv.config.js
@@ -27,25 +27,25 @@ self.__uv$config = {
         {
             host: /jav\.guru/,
             injectTo: 'body',
-            html: `<script src="/extensions/jav.guru-%20Add%20MissAV%20link%20next%20to%20Code-1.1.user.js"></script>`
+            html: `<script src="/extensions/jav-guru.user.js"></script>`
         },
         // 3. XVideos filter
         {
             host: /\.xvideos\.com/,
             injectTo: 'body',
-            html: `<script src="/extensions/XVideos%20Filter-%20Red%20K%20Black%20BG-3.1.user.js"></script>`
+            html: `<script src="/extensions/xvideos-filter.user.js"></script>`
         },
         // 4. Pornhub tweaks
         {
             host: /pornhub\.com/,
             injectTo: 'body',
-            html: `<script src="/extensions/PH%20-%20Search%20%26%20UI%20Tweaks-4.0.4.user.js"></script>`
+            html: `<script src="/extensions/ph-tweaks.user.js"></script>`
         },
         // 5. Manga loader
         {
             host: /(\.hitomi\.la|\.nhentai\.net|nhentai\.to|hitomi\.la)/,
             injectTo: 'body',
-            html: `<script src="/extensions/Manga%20Loader%20NSFW%20+%20Download-1.0.0.1.user.js"></script>`
+            html: `<script src="/extensions/manga-loader.user.js"></script>`
         }
     ]
 };
